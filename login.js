7415e750ca58d79c73c7dbfa862f535d5fab050f
Rodd Talebi <roddtalebi@gmail.com>
2018-08-25 12:37:57 -0400

    updating erg data from shell script

---
+++ login.js
@@ -0,0 +1,13 @@
+
+(function() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAiOKJVcVlS9JcTj23vUiORTMxk9qpSvwA",
+      authDomain: "website-login-3f520.firebaseapp.com",
+      databaseURL: "https://website-login-3f520.firebaseio.com",
+      projectId: "website-login-3f520",
+      storageBucket: "",
+      messagingSenderId: "82230620691"
+    };
+    firebase.initializeApp(config);
+}());
